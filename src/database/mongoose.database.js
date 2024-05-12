d44d39de12db76e5e1d34023410b3287a180e625
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-05-12 19:19:21 -0300

    Update mongoose.database.js

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
     await mongoose.connect(
-        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@taskmanagercluster.tfffvao.mongodb.net/?retryWrites=true&w=majority&appName=TaskManagerCluster`,
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@taskmanagercluster.tfffvao.mongodb.net/TaskManager?retryWrites=true&w=majority&appName=TaskManagerCluster`,
     ).then(() => console.log("Connected to MongoDB server"))
 }
 
