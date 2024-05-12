a44442e46e8578a952677853ab65cc3c39e42cbe
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-05-12 18:37:15 -0300

    feat: add route to update a task by id

--- index.js
+++ index.js
@@ -56,6 +56,32 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
+// Atualizando uma Tarefa
+app.patch('/tasks/:id', async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const taskData = req.body;
+
+        const taskToUpdate = await TaskModel.findById(taskId);
+
+        const allowedUpdates = ["isCompleted"];
+        const requestUpdates = Object.keys(taskData)
+
+        for (let update of requestUpdates) {
+            if (allowedUpdates.includes(update)) {
+                taskToUpdate[update] = taskData[update];
+            } else {
+                return res.status(500).send("Um ou mais campos inseridos não são editaveis.")
+            }
+        }
+
+        await taskToUpdate.save();
+        return res.status(200).send(taskToUpdate);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
 // Deletando uma tarefa
 app.delete('/tasks/:id', async (req, res) => {
     try {
