ebea9f96dfbe2ff60ee79c9b60bd454808f3ae50
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-05-14 08:57:53 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 const TaskRouter = require('./src/routes/task.routes');
 
 const connectToDataBase = require('./src/database/mongoose.database');
@@ -7,6 +9,7 @@ const connectToDataBase = require('./src/database/mongoose.database');
 // inicializando o express (esse "app" basicamente  possui tudo o que o servidor pode fazer)
 dotenv.config();
 const app = express();
+app.use(cors());
 
 // Falando para o express que vamos receber JSON na body das requisições
 app.use(express.json());
